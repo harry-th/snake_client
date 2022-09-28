e32170142146568116fea5e6d1c853db9e50e611
harry thomas <harryt1905@gmail.com>
2022-09-28 15:39:56 -0400

    constants

--- client.js
+++ client.js
@@ -1,10 +1,10 @@
 const net = require("net");
-
+let {port, host} = require('./constants.js');
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
+    host: host,
+    port: port,
   });
   conn.on('data',(data) => {
     console.log(data);
